eb2cb762494e55ac16ff378006ffd8cf34bffcb3
s-parvizi <sohail.parvizi@gmail.com>
2019-11-09 15:28:06 +0330

    Add some projects

---
+++ spring-test/src/main/java/com/example/springtest/SimpleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTestApplication.class, args);
+    }
+
+}
---
+++ spring-test/src/test/java/com/example/springtest/SpringTestApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.springtest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+}
