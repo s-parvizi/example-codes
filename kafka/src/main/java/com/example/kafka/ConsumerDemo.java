43f164db777aa663a8c0f0b062ec663c4a0bc98a
s-parvizi <sohail.parvizi@gmail.com>
2020-06-27 16:13:35 +0430

    Update kafka

--- kafka/src/main/java/com/example/kafka/ConsumerDemo.java
+++ kafka/src/main/java/com/example/kafka/ConsumerDemo.java
@@ -18,12 +18,12 @@ public class ConsumerDemo {
 
         Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
 
-        String bootstrapServer = "127.0.0.1:9092";
+        String bootstrapServers = "127.0.0.1:9092";
         String groupId = "my-fourth-application";
         String topic = "first_topic";
 
         Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
--- kafka/src/main/java/com/example/kafka/ConsumerDemoGroups.java
+++ kafka/src/main/java/com/example/kafka/ConsumerDemoGroups.java
@@ -18,12 +18,12 @@ public class ConsumerDemoGroups {
 
         Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class.getName());
 
-        String bootstrapServer = "127.0.0.1:9092";
+        String bootstrapServers = "127.0.0.1:9092";
         String groupId = "my-fifth-application";
         String topic = "first_topic";
 
         Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
