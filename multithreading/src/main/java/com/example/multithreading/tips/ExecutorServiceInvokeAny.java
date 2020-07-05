4673184acf8f08a88489674aa4a199991a8ec44f
s-parvizi <sohail.parvizi@gmail.com>
2020-07-05 12:42:52 +0430

    Update multithreading

---
+++ multithreading/src/main/java/com/example/multithreading/tips/ExecutorServiceInvokeAny.java
@@ -0,0 +1,28 @@
+package com.example.multithreading.tips;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ExecutorServiceInvokeAny {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newWorkStealingPool();
+
+        List<Callable<String>> callables = Arrays.asList(
+                callable("task1", 2),
+                callable("task2", 1),
+                callable("task3", 3));
+
+        String result = executor.invokeAny(callables);
+        // here the shortest task will be printed
+        System.out.println(result);
+    }
+
+    static Callable<String> callable(String result, long sleepSeconds) {
+        return () -> {
+            TimeUnit.SECONDS.sleep(sleepSeconds);
+            return result;
+        };
+    }
+}
